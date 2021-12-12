d244ae217b2e2aab0d0f5eebe9f3f4af385aa969
ajaygm312 <68458526+ajaygm312@users.noreply.github.com>
2021-12-12 19:39:28 +0530

    Create stng.ps1

---
+++ windows_config/stng.ps1
@@ -0,0 +1,3 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+& {Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0}
+& {Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1}
